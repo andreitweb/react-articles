b29159b2ea217d106a5f823c0a1481b1a6a2bdc9
Tararin Andrei <tararin@intervolga.ru>
2021-05-15 17:23:47 +0300

    Change chat's headings

--- react-cli/src/pages/ChatsPage.js
+++ react-cli/src/pages/ChatsPage.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
 
 export const ChatsPage = () => (
-  <div>Chats</div>
+  <>
+    <Typography variant="h1">Chat</Typography>
+  </>
 );
